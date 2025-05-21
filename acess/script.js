93df79c2ed0f695dcc385405c52eb567e678863b
etoniko <146545304+etoniko@users.noreply.github.com>
2025-05-21 15:22:02 +0300

    Add files via upload

---
+++ acess/script.js
@@ -0,0 +1,31 @@
+let menu = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// Typing Text Code
+
+const typed = new Typed('.multiple-text', {
+    strings: [
+  '100+ станков на складе',
+  'Доставка по всей России',
+  'Официальный продавец NUMOBAMS',
+  'Гарантия 2 года',
+  'Промышленная точность',
+  'Лучшие цены на рынке',
+  'Работаем 24/7',
+  'Проверка станка перед покупкой',
+  'Фрезер и токарка — под ключ'],
+    typeSpeed: 80,
+    backSpeed: 80,
+    backDelay: 1200,
+    loop: true,
+  });
